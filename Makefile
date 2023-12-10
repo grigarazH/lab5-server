05bb047e81286bc3b16bd65b0594bacd9f7a9166
grigarazH <grigarazh72@gmail.com>
2023-12-10 12:42:22 +0300

    Подключил докер

--- Makefile
+++
@@ -1,4 +0,0 @@
-start:
-	composer install
-	php artisan migrate --force
-	php artisan storage:link
---
+++ scripts/00-laravel-deploy.sh
@@ -0,0 +1,18 @@
+echo "Running composer"
+composer global require hirak/prestissimo
+composer install --no-dev --working-dir=/var/www/html
+
+echo "generating application key..."
+php artisan key:generate --show
+
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routs..."
+php artisan route:cache
+
+echo "Running migrations..."
+php artisan migrate --force
+
+echo "Linking storage..."
+php artisan storage:link
